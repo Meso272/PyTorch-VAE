80c4f2ebc1f9be605e74f4f3f802b61396026ad6
meso272 <liujinyang1201@126.com>
2020-10-08 17:25:29 -0700

    dd

--- cesm_compress.py
+++ cesm_compress.py
@@ -83,6 +83,8 @@ with open(args.filename, 'r') as file:
 
 model = vae_models[config['model_params']['name']](**config['model_params'])
 test = VAEXperiment(model,config['exp_params']).load_from_checkpoint(args.ckpt)
+checkpoint = torch.load(args.ckpt, map_location=lambda storage, loc: storage)
+test.load_state_dict(checkpoint['state_dict'])
 
 array=np.fromfile(args.input,dtype=np.float32)
 height=args.height
