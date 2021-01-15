e47eeff2cff84be944a724931e68c942666ee4cf
meso272 <liujinyang1201@126.com>
2021-01-14 21:05:16 -0800

    dd

--- gen_image.py
+++ gen_image.py
@@ -1,9 +1,10 @@
 import os
-folder="compress64_plus_expand"
-for file in os.listdir(folder):
+ifolder="/home/jliu447/lossycompression/PyTorch-VAE/compress64_plus_expand"
+ofolder="/home/jliu447/lossycompression/PyTorch-VAE/images"
+for file in os.listdir(ifolder):
     if file.split(".")[0][-1]!='r' and file.split(".")[0][-1]!='d':
         continue
-    i=os.path.join(folder,file)
-    o=os.path.join("images",file+".png")
+    i=os.path.join(ifolder,file)
+    o=os.path.join(ofolder,file+".png")
     comm="PlotSliceImage -f -i %s -2 3600 1800 -m INDV -n ORI -o %s" % (i,o)
     os.system(comm)
