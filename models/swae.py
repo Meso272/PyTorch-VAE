587f8997461fd167a50d73d1a58b753f161e090c
meso272 <liujinyang1201@126.com>
2021-04-01 19:07:40 -0700

    dd

--- models/swae.py
+++ models/swae.py
@@ -21,6 +21,7 @@ class SWAE(BaseVAE):
                  use_fc=True,
                  actv='leakyrelu',
                  norm='bn',
+                 struct='new',
                     **kwargs) -> None:
         super(SWAE, self).__init__()
         self.in_channels=in_channels
@@ -38,30 +39,44 @@ class SWAE(BaseVAE):
         self.last_fm_size=int( input_size/(2**len(hidden_dims)) )
         # Build Encoder
         for h_dim in hidden_dims:
-            modules.append(
+            if struct=='old':
+                modules.append(
                 nn.Sequential(
-                    nn.Conv2d(in_channels, out_channels=in_channels,
-                              kernel_size= 3, stride= 1, padding  = 1),###added layer
-                    nn.Conv2d(in_channels, out_channels=h_dim,
-                              kernel_size= 3, stride= 2, padding  = 1),
+                        nn.Conv2d(in_channels, out_channels=in_channels,
+                                  kernel_size= 3, stride= 1, padding  = 1),###added layer
+                        nn.Conv2d(in_channels, out_channels=h_dim,
+                                  kernel_size= 3, stride= 2, padding  = 1),
+                        nn.BatchNorm2d(h_dim),
+                        nn.Sequential(nn.LeakyReLU())
                     
-                    )
-            )
-            if norm=='bn':
-                modules.append(nn.Sequential(nn.BatchNorm2d(h_dim)))
-
+                        )
+                )
             else:
-                pass
+                modules.append(
+                        nn.Sequential(
+                            nn.Conv2d(in_channels, out_channels=in_channels,
+                                  kernel_size= 3, stride= 1, padding  = 1),###added layer
+                            nn.Conv2d(in_channels, out_channels=h_dim,
+                                  kernel_size= 3, stride= 2, padding  = 1),
+                    
+                        )    
+                )
+
+                if norm=='bn':
+                    modules.append(nn.Sequential(nn.BatchNorm2d(h_dim)))
+
+                else:
+                    pass
 
 
-            if actv=='leakyrelu':
-                modules.append(nn.Sequential(nn.LeakyReLU()))
-            elif actv=='prelu':
-                modules.append(nn.Sequential(nn.PReLU()))
-            elif actv=='gdn':
-                modules.append(nn.Sequential(GDN(h_dim)))
+                if actv=='leakyrelu':
+                    modules.append(nn.Sequential(nn.LeakyReLU()))
+                elif actv=='prelu':
+                    modules.append(nn.Sequential(nn.PReLU()))
+                elif actv=='gdn':
+                    modules.append(nn.Sequential(GDN(h_dim)))
         
-            in_channels = h_dim
+                in_channels = h_dim
 
         self.encoder = nn.Sequential(*modules)
         if self.use_fc:
@@ -74,45 +89,68 @@ class SWAE(BaseVAE):
             self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * self.last_fm_size*self.last_fm_size)
 
         hidden_dims.reverse()
-
+   
         for i in range(len(hidden_dims) - 1):
-            modules.append(
-                nn.Sequential(
-                    nn.ConvTranspose2d(hidden_dims[i],
-                                       hidden_dims[i],
-                                       kernel_size=3,
-                                       stride = 1,
-                                       padding=1,
-                                       output_padding=0),##added layer
-                    nn.ConvTranspose2d(hidden_dims[i],
-                                       hidden_dims[i + 1],
-                                       kernel_size=3,
-                                       stride = 2,
-                                       padding=1,
-                                       output_padding=1),
+            if struct=='old':
+                modules.append(
+                    nn.Sequential(
+                        nn.ConvTranspose2d(hidden_dims[i],
+                                           hidden_dims[i],
+                                           kernel_size=3,
+                                           stride = 1,
+                                           padding=1,
+                                           output_padding=0),##added layer
+                        nn.ConvTranspose2d(hidden_dims[i],
+                                           hidden_dims[i + 1],
+                                           kernel_size=3,
+                                           stride = 2,
+                                           padding=1,
+                                           output_padding=1),
+                        nn.BatchNorm2d(hidden_dims[i + 1]),
+                        nn.LeakyReLU()
+
                     )
             )
-
-            if norm=='bn':
-                modules.append(nn.Sequential(nn.BatchNorm2d(hidden_dims[i + 1])))
-
             else:
-                pass
 
 
-            if actv=='leakyrelu':
-                modules.append(nn.Sequential(nn.LeakyReLU()))
-            elif actv=='prelu':
-                modules.append(nn.Sequential(nn.PReLU()))
-            elif actv=='gdn':
-                modules.append(nn.Sequential(GDN(hidden_dims[i + 1],inverse=True)))
+                modules.append(
+                    nn.Sequential(
+                        nn.ConvTranspose2d(hidden_dims[i],
+                                           hidden_dims[i],
+                                           kernel_size=3,
+                                           stride = 1,
+                                           padding=1,
+                                           output_padding=0),##added layer
+                        nn.ConvTranspose2d(hidden_dims[i],
+                                           hidden_dims[i + 1],
+                                           kernel_size=3,
+                                           stride = 2,
+                                           padding=1,
+                                           output_padding=1),
+                        )
+                )
+
+                if norm=='bn':
+                    modules.append(nn.Sequential(nn.BatchNorm2d(hidden_dims[i + 1])))
+
+                else:
+                    pass
+
+
+                if actv=='leakyrelu':
+                    modules.append(nn.Sequential(nn.LeakyReLU()))
+                elif actv=='prelu':
+                    modules.append(nn.Sequential(nn.PReLU()))
+                elif actv=='gdn':
+                    modules.append(nn.Sequential(GDN(hidden_dims[i + 1],inverse=True)))
 
 
 
         self.decoder = nn.Sequential(*modules)
-        modules=[]
-
-        modules.append ( nn.Sequential(
+        
+        if struct=='old':
+            self.final_layer_1= nn.Sequential(
                             nn.ConvTranspose2d(hidden_dims[-1],
                                        hidden_dims[-1],
                                        kernel_size=3,
@@ -125,23 +163,45 @@ class SWAE(BaseVAE):
                                                stride=2,
                                                padding=1,
                                                output_padding=1),
+                            nn.BatchNorm2d(hidden_dims[-1]),
+                            nn.LeakyReLU()
                             
-                            ) )
-        if norm=='bn':
-            modules.append(nn.Sequential(nn.BatchNorm2d(hidden_dims[-1])))
+                            ) 
+
+
 
         else:
-            pass
+            modules=[]
+            modules.append ( nn.Sequential(
+                                nn.ConvTranspose2d(hidden_dims[-1],
+                                           hidden_dims[-1],
+                                           kernel_size=3,
+                                           stride = 1,
+                                           padding=1,
+                                           output_padding=0),##added layer
+                                nn.ConvTranspose2d(hidden_dims[-1],
+                                                   hidden_dims[-1],
+                                                   kernel_size=3,
+                                                   stride=2,
+                                                   padding=1,
+                                                   output_padding=1),
+                            
+                                ) )
+            if norm=='bn':
+                modules.append(nn.Sequential(nn.BatchNorm2d(hidden_dims[-1])))
+
+            else:
+                pass
 
 
-        if actv=='leakyrelu':
-            modules.append(nn.Sequential(nn.LeakyReLU()))
-        elif actv=='prelu':
-            modules.append(nn.Sequential(nn.PReLU()))
-        elif actv=='gdn':
-            modules.append(nn.Sequential(GDN(hidden_dims[-1],inverse=True)))
+            if actv=='leakyrelu':
+                modules.append(nn.Sequential(nn.LeakyReLU()))
+            elif actv=='prelu':
+                modules.append(nn.Sequential(nn.PReLU()))
+            elif actv=='gdn':
+                modules.append(nn.Sequential(GDN(hidden_dims[-1],inverse=True)))
 
-        self.final_layer_1=nn.Sequential(*modules)
+            self.final_layer_1=nn.Sequential(*modules)
 
         self.final_layer_2=nn.Sequential(nn.Conv2d(hidden_dims[-1], out_channels= self.in_channels,
                                       kernel_size= 3, padding= 1),
