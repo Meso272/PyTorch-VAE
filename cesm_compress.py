66d24d921f411d385a664c6bc1a94cd82894493d
meso272 <liujinyang1201@126.com>
2021-04-02 17:26:27 -0700

    dd

--- cesm_compress.py
+++ cesm_compress.py
@@ -94,6 +94,7 @@ model = vae_models[config['model_params']['name']](**config['model_params'])
 test = VAEXperiment(model,config['exp_params'])
 checkpoint = torch.load(args.ckpt, map_location=lambda storage, loc: storage)
 test.load_state_dict(checkpoint['state_dict'])
+test=test.model
 test=test.cuda()
 test.eval()
 
