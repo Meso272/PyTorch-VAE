45e88af6ba5cae3b5997d84aa529ecb471b1cc93
meso272 <liujinyang1201@126.com>
2021-04-11 16:09:50 -0700

    12

---
+++ calc_diff.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np 
+
+dtype=sys.argv[1]
+
+if dtype=="i":
+    dtype=np.int32
+else:
+    dtype=np.float32
+
+ori=sys.argv[2]
+tar=sys.argv[3]
+
+o=np.fromfile(ori,dtype=dtype)
+n=o.shape[0]
+t=np.zeros((n,),dtype=dtype)
+last=0
+for i in range(n):
+    t[i]=n[i]-last
+    last=n[i]
+t=t+np.min(t)
+t.tofile(tar)
