1523479a30fc27c54dfd00fc2fb1f156f6d8544c
meso272 <liujinyang1201@126.com>
2021-04-11 00:27:05 -0700

    12

--- resume.py
+++ resume.py
@@ -24,7 +24,7 @@ if __name__=='__main__':
                     help =  'path to the checkpoint file',
                     )
     args = parser.parse_args()
-    with open(args.config, 'r') as file:
+    with open(args.filename, 'r') as file:
         try:
             config = yaml.safe_load(file)
         except yaml.YAMLError as exc:
