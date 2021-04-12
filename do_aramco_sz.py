77af186fc88f5c1f0500184147d7bdd87508ffd9
meso272 <liujinyang1201@126.com>
2021-04-12 16:48:13 -0700

    12

---
+++ do_aramco_sz.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np 
+
+datafolder="/home/jliu447/lossycompression/aramco"
+
+ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
+
+cr=np.zeros((29,11),dtype=np.float32)
+psnr=np.zeros((29,11),dtype=np.float32)
+for i,eb in enumerate(ebs):
+    cr[i+1][0]=eb
+    psnr[i+1][0]=eb
+    for j in range(1510,1601,10):
+        cr[0][j-51]=j
+        psnr[0][j-51]=j
+        filename="aramco-snapshot-%s.f32" % str(j)
+        filepath=os.path.join(datafolder,filename)
+        comm="sz -z -f -i %s -M REL -R %f -3 235 449 449" % (filepath,eb)
+        os.system(comm)
+        szpath=filepath+".sz"
+        comm="sz -x -f -i %s -s %s -3 235 449 449 -a>temp.txt" % (filepath,szpath)
+        os.system(comm)
+        with open("temp.txt","r") as f:
+            lines=f.read().splitlines()
+            p=eval(lines[4].split(',')[0].split('=')[1])
+            
+            r=eval(lines[7].split('=')[1])
+            
+            cr[i+1][j-51]=r
+            psnr[i+1][j-51]=p
+
+        comm="rm -f %s" % szpath
+        os.system(comm)
+        comm="rm -f %s" % szpath+".out"
+        os.system(comm)
+        os.system("rm -f temp.txt")
+
+
+np.savetxt("sz_aramco_cr.txt",cr,delimiter='\t')
+np.savetxt("sz_aramco_psnr.txt",psnr,delimiter='\t')
---
+++ do_aramco_zfp.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np 
+
+datafolder="/home/jliu447/lossycompression/aramco"
+
+ebs=[0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+
+cr=np.zeros((len(ebs)+1,11),dtype=np.float32)
+psnr=np.zeros((len(ebs)+1,11),dtype=np.float32)
+maxpwerr=np.zeros((len(ebs)+1,11),dtype=np.float32)
+
+for i,eb in enumerate(ebs):
+    eb=2**eb
+    cr[i+1][0]=eb
+    psnr[i+1][0]=eb
+    maxpwerr[i+1][0]=eb
+    for j in range(1510,1601,10):
+        cr[0][j-51]=j
+        psnr[0][j-51]=j
+        maxpwerr[0][j-51]=j
+        filename="aramco-snapshot-%s.f32" % str(j)
+        filepath=os.path.join(datafolder,filename)
+        arr=np.fromfile(filepath,dtype=np.float32)
+        rng=np.max(arr)-np.min(arr)
+        comm="zfp -s -i %s -z out.dat -f -3 235 449 449 -a %f &>temp.txt" % (filepath,eb)
+        os.system(comm)
+        
+        
+        with open("temp.txt","r") as f:
+            line=f.read()
+            
+            #print(line[0])
+            p=eval(line.split(' ')[12].split('=')[1])
+            
+            r=eval(line.split(' ')[7].split('=')[1])
+
+            e=eval(line.split(' ')[11].split('=')[1])/rng
+            #print(p)
+            #print(r)
+            #print(e)
+            cr[i+1][j-51]=r
+            psnr[i+1][j-51]=p
+            maxpwerr[i+1][j-51]=e
+
+        os.system("rm -f out.dat")
+        os.system("rm -f temp.txt")
+
+
+np.savetxt("zfp_aramco_cr.txt",cr,delimiter='\t')
+np.savetxt("zfp_aramco_psnr.txt",psnr,delimiter='\t')
+np.savetxt("zfp_aramco_maxpwerr.txt",maxpwerr,delimiter='\t')
