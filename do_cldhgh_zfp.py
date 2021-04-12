205522ba5bb36df55348985ded67b8ba2f5fc277
meso272 <liujinyang1201@126.com>
2021-04-12 02:02:03 -0700

    12

--- do_cldhgh_zfp.py
+++ do_cldhgh_zfp.py
@@ -29,7 +29,7 @@ for i,eb in enumerate(ebs):
         with open("temp.txt","r") as f:
             line=f.read()
             print(line)
-            print(line.split(' '))
+            print(line[10])
             p=eval(line.split(' ')[12].split('=')[1])
             
             r=eval(line.split(' ')[7].split('=')[1])
