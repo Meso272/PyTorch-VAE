6a72ecccd9cf3b7306a42a04b0234f9c7a502d24
meso272 <liujinyang1201@126.com>
2021-04-15 05:28:10 -0700

    12

---
+++ calc_hurricane_ranges.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np 
+
+datafolder="/home/jliu447/lossycompression/Hurricane/clean-data-Jinyang"
+
+#ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
+fields=["CLOUD","PRECIP","QGRAUP","QRAIN","QVAPOR","U","W","P","QCLOUD","QICE","QSNOW","TC","V"]
+
+maxs=[]
+mins=[]
+for field in enumerate(fields):
+    
+    for i in range(1,49):
+        s=str(i)
+        if i<10:
+            s='0'+s
+        
+        
+        filename="%sf%s.bin" % str(field,s)
+        filepath=os.path.join(datafolder,filename)
+        a=np.fromfile(filepath,dtype=np.float32)
+
+        if i==0:
+            mi=np.min(a)
+            mx=np.max(a)
+        else:
+            mi=min(mi,np.min(a))
+            mx=max(np.max(a))
+    maxs.append(mx)
+    mins.append(mi)
+
+print(maxs)
+print(mins)
+
