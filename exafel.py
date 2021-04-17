033a0536f4744cbd260e3a02d13d87daef2ba9c7
meso272 <liujinyang1201@126.com>
2021-04-17 05:08:03 -0700

    12

--- exafel.py
+++ exafel.py
@@ -3,37 +3,36 @@ import numpy as np
 import os
 class EXAFEL(Dataset):
     def __init__(self,path,start,end,size,global_max,global_min,epsilon=-1):
-        height=185
+        height=185*32
         width=388
         picts=[]
         for i in range(start,end):
             
             filename="group_renamed_%d.dat" % i
             filepath=os.path.join(path,filename)
-            array_32=np.fromfile(filepath,dtype=np.float32).reshape((32,height,width))
+            array=np.fromfile(filepath,dtype=np.float32).reshape((32,height,width))
 
         #print(array)
-            for i in range(32):
-                array=array_32[i]
-                for x in range(0,height,size):
-                    for y in range(0,width,size):
-                        endx=min(x+size,height)
-                        endy=min(y+size,width)
-                        pict=array[x:endx,y:endy]
-                        padx=size-pict.shape[0]
-                        pady=size-pict.shape[1]
+            
+            for x in range(0,height,size):
+                for y in range(0,width,size):
+                    endx=min(x+size,height)
+                    endy=min(y+size,width)
+                    pict=array[x:endx,y:endy]
+                    padx=size-pict.shape[0]
+                    pady=size-pict.shape[1]
 
-                        pict=(pict-global_min)/(global_max-global_min)
+                    pict=(pict-global_min)/(global_max-global_min)
 
-                        pict=np.pad(pict,((0,padx),(0,pady)))
-                        pict=pict*2-1
-                        if epsilon>0:
-                            v=np.var(pict)
-                            if v<=epsilon:
-                                continue
-                        pict=np.expand_dims(pict,0)
+                    pict=np.pad(pict,((0,padx),(0,pady)))
+                    pict=pict*2-1
+                    if epsilon>0:
+                        v=np.var(pict)
+                        if v<=epsilon:
+                            continue
+                    pict=np.expand_dims(pict,0)
                     #print(array[x:x+size,y:y+size])
-                        picts.append(pict)
+                    picts.append(pict)
         self.picts=np.array(picts)
     def __len__(self):
         return self.picts.shape[0]
