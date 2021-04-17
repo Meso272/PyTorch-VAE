4470f961c5edccab3850a92cdf32933304d69a4e
meso272 <liujinyang1201@126.com>
2021-04-17 00:12:48 -0700

    12

---
+++ check_exafel.py
@@ -0,0 +1,23 @@
+import os
+crs=[]
+for i in range(986):
+    filename="group_%d.dat" % i
+    szname=filename+".sz"
+    os.system("sz -z -f -i %s -M REL -3 388 185 32 -R 1e-2&>f.txt" % filename)
+    os.system("sz -x -f -s %s -3 388 185 32 -a -i %s&>temp.txt" %(szname,filename))
+    with open("temp.txt","r") as f:
+        lines=f.read().splitlines()
+        
+            
+        r=eval(lines[7].split('=')[1])
+        crs.append(r)
+    os.system("rm -f *sz*")
+    os.system("rm -f temp.txt;rm -f f.txt")
+
+with open("crs.txt",'r') as f:
+    for i in range(986):
+        f.write(str(i)+"\t"+str(cr[i])+"\n")
+
+    
+        
+       
