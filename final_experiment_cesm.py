797b1acf7add05db87da5fae0990f15cd4044af4
meso272 <liujinyang1201@126.com>
2021-04-23 22:20:35 -0700

    12

--- final_experiment_cesm.py
+++ final_experiment_cesm.py
@@ -7,8 +7,8 @@ field=sys.argv[3]
 blocksize=int(sys.argv[4])
 coeff=float(sys.argv[5])
 output=sys.argv[6]
-#ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
-ebs=[1e-2,1e-3]
+ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
+#ebs=[1e-2,1e-3]
 idxrange=[x for x in range(52,63)]
 datafolder="/home/jliu447/lossycompression/cesm-multisnapshot-5fields/%s" % field
 
