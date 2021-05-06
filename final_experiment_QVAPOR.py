26913eba0b211f486262071ad87beb78c756e883
meso272 <liujinyang1201@126.com>
2021-05-06 01:23:03 -0700

    dd

--- final_experiment_QVAPOR.py
+++ final_experiment_QVAPOR.py
@@ -19,10 +19,10 @@ if len(sys.argv)>=9:
     else:
         latent_rate=int(sys.argv[8])
 print(eps)
-#ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
-ebs=[1e-2,1e-3]
-#idxrange=[x for x in range(41,49)]
-idxrange=[41,42]
+ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
+#ebs=[1e-2,1e-3]
+idxrange=[x for x in range(41,49)]
+#idxrange=[41,42]
 datafolder="/home/jliu447/lossycompression/Hurricane/clean-data-Jinyang" 
 pid=str(os.getpid()).strip()
 data=np.zeros((len(ebs)+1,len(idxrange)+1,5),dtype=np.float32)
