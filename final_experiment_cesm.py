26bd3660f456ce0b51568d98ee3c2366f97e3540
meso272 <liujinyang1201@126.com>
2021-05-12 18:40:11 -0700

    dd

--- final_experiment_cesm.py
+++ final_experiment_cesm.py
@@ -19,8 +19,8 @@ if len(sys.argv)>=9:
        preset_latent_rate=float(sys.argv[8])
 
 #ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
-#ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]
-ebs=[1e-2]
+ebs=[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,10)]
+#ebs=[1e-2]
 idxrange=[x for x in range(52,63)]
 #idxrange=[52,53]
 datafolder="/home/jliu447/lossycompression/cesm-multisnapshot-5fields/%s" % field
