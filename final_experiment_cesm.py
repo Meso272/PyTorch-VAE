5146adc884aa78ae5f066206d6348b3a7690fe1e
meso272 <liujinyang1201@126.com>
2021-05-16 06:44:22 -0700

    dd

--- final_experiment_cesm.py
+++ final_experiment_cesm.py
@@ -18,7 +18,7 @@ if len(sys.argv)>=9:
     else:
        preset_latent_rate=float(sys.argv[8])
 
-ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,4)]#+[i*1e-2 for i in range(1,11)]
+ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,4)]+[i*1e-2 for i in range(1,11)]
 #ebs=[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,10)]
 #ebs=[i*1e-4 for i in range(1,10)]
 #ebs=[1e-2]
