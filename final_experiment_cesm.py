8e8f747c5b7f7d98bbd070c43f627c90b370c167
meso272 <liujinyang1201@126.com>
2021-05-16 05:53:41 -0700

    dd

--- final_experiment_cesm.py
+++ final_experiment_cesm.py
@@ -80,9 +80,10 @@ for j,idx in enumerate(idxrange):
                             try:
 
                                 lines=f.read().splitlines()
+                                print(lines)
                                 latent_cr=eval(lines[7].split("=")[-1])
                             except:
-                                print(lines)
+                                
                                 latent_cr=0
                         os.system("rm -f %s_t2.5.txt" % pid)
                         os.system("rm -f %s*sz3*")
