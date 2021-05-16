658980a2ee68baeabccf342ba5ab7c92e8124b18
meso272 <liujinyang1201@126.com>
2021-05-16 06:24:07 -0700

    dd

--- final_experiment_aramco.py
+++ final_experiment_aramco.py
@@ -20,9 +20,9 @@ if len(sys.argv)>=9:
     else:
         preset_latent_rate=float(sys.argv[8])
 print(eps)
-ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
+#ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
 #ebs=[1e-2]
-#ebs=[i*1e-4 for i in range(1,10)]#+[i*1e-3 for i in range(1,10)]
+ebs=[i*1e-4 for i in range(1,10)]#+[i*1e-3 for i in range(1,10)]
 idxrange=[x for x in range(1510,1600,10)]+[1599]
 #idxrange=[1520,1540]
 datafolder="/home/jliu447/lossycompression/aramco" 
